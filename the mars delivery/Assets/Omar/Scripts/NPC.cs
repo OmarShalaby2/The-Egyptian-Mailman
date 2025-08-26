84d9269c76c01277a2753de24e5c821e31ebf1ec
Omar Shalaby <ahero2004@gmail.com>
2025-08-26 22:30:10 +0300

    NPC Dialogue

---
+++ the mars delivery/Assets/Omar/Scripts/NPC.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    public NPCDialogue dialogueData;
+    public GameObject dialoguePanel;
+    public TMP_Text dialogueText, nameText;
+    public Image portraitImage;
+
+    private int dialogueIndex;
+    private bool isTyping, isDialogueActive;
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && Input.GetKeyDown(KeyCode.E) && canInteract())
+        {
+            Interact();
+        }
+    }
+
+    public bool canInteract()
+    {
+        return !isDialogueActive;
+    }
+
+    public void Interact()
+    {
+        if (dialogueData == null)
+        {
+            return;
+        }
+
+        if(isDialogueActive)
+        {
+            NextLine();
+        }
+        else
+        {
+            StartDialogue();
+        }
+    }
+
+    void StartDialogue()
+    {
+        isDialogueActive = true;
+        dialogueIndex = 0;
+
+        nameText.SetText(dialogueData.npcName);
+        portraitImage.sprite = dialogueData.ncpIcon;
+
+        dialoguePanel.SetActive(true);
+
+        StartCoroutine(TypeLine());
+    }
+
+    void NextLine()
+    {
+        if (isTyping)
+        {
+            StopAllCoroutines();
+            dialogueText.SetText(dialogueData.dialogueLines[dialogueIndex]);
+            isTyping = false;
+        }
+        else if(++dialogueIndex < dialogueData.dialogueLines.Length)
+        {
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            EndDialogue();
+        }
+    }
+
+    IEnumerator TypeLine()
+    {
+        isTyping = true;
+        dialogueText.SetText("");
+
+        foreach (char letter in dialogueData.dialogueLines[dialogueIndex])
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(dialogueData.typingSpeed);
+        }
+
+        isTyping = false;
+
+        if(dialogueData.autoProgressLines.Length > dialogueIndex && dialogueData.autoProgressLines[dialogueIndex])
+        {
+            yield return new WaitForSeconds(dialogueData.AutoProgressDelay);
+            NextLine();
+        }
+    }
+
+    public void EndDialogue()
+    {
+        StopAllCoroutines();
+        isDialogueActive = false;
+        dialogueText.SetText("");
+        dialoguePanel.SetActive(false);
+    }
+}
